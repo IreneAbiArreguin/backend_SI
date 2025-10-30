8251c86d929a95f2edda71e2a1f59bad4cebc023
AbiEsp <irene.arreguin.03.29@gmail.com>
2025-10-30 16:27:46 -0500

    creacion cors

---
+++ backend/config/cors.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    
+    'allowed_methods' => ['*'],
+    
+    'allowed_origins' => ['*'],  // En producción usa tu dominio específico
+    
+    'allowed_origins_patterns' => [],
+    
+    'allowed_headers' => ['*'],
+    
+    'exposed_headers' => [],
+    
+    'max_age' => 0,
+    
+    'supports_credentials' => false,
+];
